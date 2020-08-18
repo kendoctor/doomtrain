288e2cb50c3ec27a48471d57801a5c8e44cb5e63
kendoctor <kendoctor@163.com>
2020-08-18 11:58:45 +0800

    extended CarriageFactory from AbstractFactory and use singleton for instancing

--- facto/train/carriagefactory.lua
+++ facto/train/carriagefactory.lua
@@ -1,64 +1,43 @@
 local Class = require("oop.class")
--- local Map = require("oop.map")
+local AbstractFactory = require("facto.abstractfactory")
 
-CarriageFactory = Class.create() -- persisted 
-local registered = {}
-local instanced = {}
-CarriageFactory.registered = registered
-CarriageFactory.instanced = instanced
+--- Carriage factory for creating different types of carriages, such as locomotive, cargo wagon, etc.
+-- Centralized management of carriage instances for facto data serialization
+-- @classmod CarriageFactory
+CarriageFactory = Class.extend({}, AbstractFactory)
 
-function CarriageFactory.register(carriage_type, class)
-    registered[carriage_type] = class
-end 
-
-function CarriageFactory.getClass(carriage_type)
-    return registered[carriage_type]
-end 
-
-function CarriageFactory.create(carriage_type, props)
-    local class = CarriageFactory.getClass(carriage_type)
-    if class == nil then error(string.format("Carriage type(%s) not supported", carriage_type)) end 
-    local instance = class(props)
-    instance.type = carriage_type 
-    instanced[tostring(instance:getId())] = instance
-    return instance
-end 
-
-function CarriageFactory.get(id)    
-    return instanced[tostring(id)]
-end 
-
-function CarriageFactory.remove(carriage)
+--- Remove carriage from cache.
+-- Check if the same id carriage already replaced before removed 
+-- @tparam class<Carriage> carriage
+function CarriageFactory:remove(carriage)
     local id = tostring(carriage:getId())
-    local compare = instanced[id]
-    if compare == carriage then instanced[id] = nil end 
+    local compare = self.instanced[id]
+    if compare == carriage then self.instanced[id] = nil end 
 end 
 
-function CarriageFactory.setup()
+--- Setup carriage factory.
+function CarriageFactory:setup()
     local class = require("facto.train.carriage")
-    CarriageFactory.register(class.type, class)
+    self:register(class.type, class)
     class = require("facto.train.cargowagon")
-    CarriageFactory.register(class.type, class)    
+    self:register(class.type, class)    
 end 
 
-function CarriageFactory.init(guid, storage)
-    storage.instanced = instanced
-end 
-
-function CarriageFactory.load(guid, data)
-    instanced = data.instanced
-    for id, data in pairs(instanced) do 
-        local class = CarriageFactory.getClass(data.type)
-        if class == nil then error(string.format("Carriage type(%s) not supported", data.type)) end 
-        class:__metalize(data)
-    end 
-end 
-
+--- Return a global unique key
+-- @treturn string
 function CarriageFactory.guid()
     return "facto.train.carriagefactory"
 end 
 
-CarriageFactory.setup()
+local instance 
+--- Get singleton instance
+function CarriageFactory.getInstance()
+    if instance == nil then 
+       instance = CarriageFactory()
+       instance:setup()
+    end 
+    return instance
+end 
 
 -- @export
 return CarriageFactory
