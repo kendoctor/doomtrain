4d91c2685c385178ed12e333902f19f290a4bbb3
kendoctor <kendoctor@163.com>
2020-08-18 13:03:40 +0800

    fixed factory usage due to using singleton pattern

--- facto/gamemanager.lua
+++ facto/gamemanager.lua
@@ -1,10 +1,13 @@
 require "utils"
 local Class = require("oop.class")
 local Event = require("facto.event")
+
+--- Manage the game objects and resources.
+-- @classmod GameManager
 local GameManager = Class.create()
 local persisted_classes = {}
-GameManager.persisted_classes = persisted_classes
 
+-- GameManager.persisted_classes = persisted_classes
 -- config : new game cfg, and cfg could be changed in game
 --- modules should be registered 
 --- modules could be registered lately
@@ -20,41 +23,39 @@ function GameManager.setup(cfg)
     --     end 
     -- end 
     local class = require("facto.train.trainfactory")
-    persisted_classes[class.guid()] = class
+    GameManager.register(class)
     class = require("facto.train.carriagefactory")
-    persisted_classes[class.guid()] = class
+    GameManager.register(class)    
     class = require("facto.train.carriagedoormanager")
-    persisted_classes[class.guid()] = class
+    GameManager.register(class)
 end 
 
+--- Register persisted class singleton.
 function GameManager.register(class)
-    -- assert(Class.isclass(class))
-    local guid = class.guid()
-    if persisted_classes[guid] ~= nil then 
-        persisted_classes[guid] = class
-    end 
+    -- assert(Class.isclass(class))    
+    persisted_classes[class.guid()] = class.getInstance()
 end 
 
+--- Invoked when script.on_init triggered.
 function GameManager.init()
     -- via cfg. init feature     
     global.facto = global.facto or {} 
-    for guid, class in pairs(persisted_classes) do
+    for guid, persisted in pairs(persisted_classes) do
         -- if global.facto[guid] == nil, that means some features added
-        -- should not change this data in on load
-        assert(guid == class.guid())
+        -- should not change this data in on load        
         if global.facto[guid] == nil then global.facto[guid] = {} end
-        class.init(guid, global.facto[guid])
+        persisted:init(guid, global.facto[guid])
     end     
 end 
 
+-- Inovked when script.on_load triggered
 function GameManager.load()   
     log(serpent.block(global.facto, {comment=true}))
     -- if global.facto == nil, that means game already broken
-    for guid, class in pairs(persisted_classes) do
+    for guid, persisted in pairs(persisted_classes) do
         -- if global.facto[guid] == nil, that means some features added
-        -- should not change this data in on load
-        assert(guid == class.guid())
-        class.load(guid, global.facto[guid])
+        -- should not change this data in on load        
+        persisted:load(guid, global.facto[guid])
     end 
 end 
 
@@ -62,12 +63,11 @@ end
 -- 1. on_init will not trigger, if not scenario changed ?
 -- on_configuration_changed ? need to test this event when will be triggered
 -- local Train = GlobalObjectManager.createSerializableClass("facto.train", {}, SelfCollection)
-function GameManager.run()
-    GameManager.setup(cfg)
+function GameManager.run()    
+    GameManager.setup(cfg)    
     Event.on_init(function()
         GameManager.init()
     end)
-
     Event.on_load(function()
         GameManager.load()
     end)
