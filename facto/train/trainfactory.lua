c7beb5b55a53348593c45dd74c6b663cd83d6f33
kendoctor <kendoctor@163.com>
2020-08-18 11:48:28 +0800

    extended TrainFactory from AbstractFactory and use singleton pattern for instancing

--- facto/train/trainfactory.lua
+++ facto/train/trainfactory.lua
@@ -1,59 +1,35 @@
--- TrainFactory Module
 local Class = require("oop.class")
--- local Event = require("facto.event")
+local AbstractFactory = require("facto.abstractfactory")
 
-TrainFactory = Class.create() -- persisted 
-local registered = {}
-local instanced = {}
-TrainFactory.registered = registered
-TrainFactory.instanced = instanced
+--- Train factory for creating different types of train
+-- Centralized management of train instances for facto data serialization
+-- @classmod TrainFactory
+local TrainFactory = Class.extend({}, AbstractFactory)
 
-function TrainFactory.register(train_type, class)
-    registered[train_type] = class
-end 
-
-function TrainFactory.getClass(train_type)
-    return registered[train_type]
-end 
-
-function TrainFactory.create(train_type, props)
-    local class = TrainFactory.getClass(train_type)
-    if class == nil then error(string.format("Train type(%s) not supported", train_type)) end 
-    local instance = class(props)  
-    instance.type = train_type 
-    instanced[tostring(instance:getId())] = instance
-    return instance
-end 
-
-function TrainFactory.remove(train)    
-    instanced[tostring(train:getId())] = nil 
-end 
-
-function TrainFactory.setup()
+--- Setup train factory
+-- @todo using cfg
+function TrainFactory:setup()    
     local class = require("facto.train.train")
-    TrainFactory.register(class.type, class)
+    self:register(class.type, class)
 end 
 
-function TrainFactory.init(guid, storage)
-    storage.instanced = instanced
-end 
+-- @section static members
 
-function TrainFactory.load(guid, data)
-    -- log(serpent.block(data))
-    instanced = data.instanced
-    for id, data in pairs(instanced) do 
-        local class = TrainFactory.getClass(data.type)
-        if class == nil then error(string.format("Train type(%s) not supported", data.type)) end 
-        class:__metalize(data)
-    end 
-end 
-
+--- Return a global unique key
+-- @treturn string
 function TrainFactory.guid()
     return "facto.train.trainfactory"
 end 
 
-
-TrainFactory.setup()
+local instance 
+--- Get singleton instance
+function TrainFactory.getInstance()
+    if instance == nil then 
+       instance = TrainFactory()
+       instance:setup()      
+    end 
+    return instance
+end 
 
 -- @export
 return TrainFactory
