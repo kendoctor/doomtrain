78a70e039a5a82b0b101be009cc520771f2af7d4
kendoctor <kendoctor@163.com>
2020-08-18 12:08:05 +0800

    extended CarriageDoorManager from AbstractFactory and use singleton for instancing

--- facto/train/carriagedoormanager.lua
+++ facto/train/carriagedoormanager.lua
@@ -1,54 +1,59 @@
 local Class = require("oop.class")
--- local Map = require("oop.map")
+local AbstractFactory = require("facto.abstractfactory")
 local CarriageDoor = require("facto.train.carriagedoor")
 
-CarriageDoorManager = Class.create() -- persisted 
-local instanced = {}
-CarriageDoorManager.instanced = instanced
+--- Carriage door manager for centralized management of door instances and facto data serialization.
+-- @classmod CarriageDoorManager
+CarriageDoorManager = Class.extend({}, AbstractFactory)
 
-
-function CarriageDoorManager.create(props)    
+--- Create carriage door with properties.
+-- @tparam table props
+-- @treturn class<CarriageDoor>
+function CarriageDoorManager:create(props)    
     local instance = CarriageDoor(props)    
-    instanced[tostring(instance:getId())] = instance
+    self.instanced[tostring(instance:getId())] = instance
     return instance
 end 
 
--- lazycall mode
-function CarriageDoorManager.createLazyAdded(props)    
+--- Create carriage door in lazy added mode.
+-- Considering creating facto entities and tiles in one call
+-- When in final creation of all entities, invoke the returned closure
+-- @tparam table props
+-- @treturn class<CarriageDoor>, closure 
+function CarriageDoorManager:createLazyAdded(props)    
     local instance = CarriageDoor(props)    
-    return instance, function() instanced[tostring(instance:getId())] = instance end 
+    return instance, function() self.instanced[tostring(instance:getId())] = instance end 
 end 
 
-function CarriageDoorManager.get(id)
-    return instanced[tostring(id)]
+--- Setup 
+function CarriageDoorManager:setup()
 end 
 
-function CarriageDoorManager.remove(object)
-    local id = tostring(object:getId())
-    local instance = instanced[id]
-    -- if instance then instance:destory() end 
-    instanced[id] = nil
-end 
-
-function CarriageDoorManager.setup()
-end 
-
-function CarriageDoorManager.init(guid, storage)
-    storage.instanced = instanced
-end 
-
-function CarriageDoorManager.load(guid, data)
-    instanced = data.instanced
-    for id, data in pairs(instanced) do 
+--- Metalize doors when game save loaded.
+-- @tparam string guid
+-- @tparam table data
+function CarriageDoorManager:load(guid, data)
+    self.instanced = data.instanced
+    for id, data in pairs(self.instanced) do 
         CarriageDoor:__metalize(data)
     end 
 end 
 
+--- Return a global unique key.
+-- @treturn string
 function CarriageDoorManager.guid()
     return "facto.train.carriagedoormanager"
 end 
 
-CarriageDoorManager.setup()
+local instance 
+--- Get singleton instance.
+function CarriageDoorManager.getInstance()
+    if instance == nil then 
+       instance = CarriageDoorManager()
+       instance:setup()
+    end 
+    return instance
+end 
 
 -- @export
 return CarriageDoorManager
