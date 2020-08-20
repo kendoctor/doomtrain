f2c84a0e5d46db93f71b0bc8449dcda88e73e908
kendoctor <kendoctor@163.com>
2020-08-20 21:20:54 +0800

    doc added for Class CargoWagon

--- facto/train/cargowagon.lua
+++ facto/train/cargowagon.lua
@@ -3,19 +3,24 @@ local Event = require("facto.event")
 local Carriage = require("facto.train.carriage")
 local exchangerFactory = require("facto.exchanger.exchangerfactory").getInstance()
 
+--- Cargo wagon implementation.
+-- @classmod CargoWagon
 local CargoWagon = Class.extend({}, Carriage)
+-- @property members
+-- @property string the type of carriage which is for factory registration.
 CargoWagon.type = "cargo-wagon"
+-- @property table table for holds exchangers for the carriage.
+-- Note: here should be nil, only for property definition. 
+CargoWagon.exchangers = nil 
 
+-- @metatable members
+--- Initialization.
 function CargoWagon:initialize()
     self.exchangers = {}
 end 
 
-function CargoWagon:isLocomotive()
-    return false 
-end 
-
---- Build carriage such room, doors.
--- @tparam class<Carriage> old_carriage
+--- Build carriage such as room, doors, exchangers.
+-- @tparam class<CargoWagon> old_carriage
 -- @tparam table tiles a reference table for caching tiles
 -- @tparam table lazycalls a reference table for caching closures
 function CargoWagon:build(old_carriage, tiles, lazycalls)
@@ -31,7 +36,7 @@ function CargoWagon:build(old_carriage, tiles, lazycalls)
 end 
 
 --- Clone carriage from the old.
--- @tparam class<Carriage> old_carriage
+-- @tparam class<CargoWagon> old_carriage
 function CargoWagon:clone(old_carriage)
     local restore = function(e)
         for k, exchanger in pairs(old_carriage.exchangers) do 
@@ -51,28 +56,9 @@ function CargoWagon:clone(old_carriage)
     Event.remove(defines.events.on_entity_cloned, restore)
 end 
 
--- function CargoWagon:findExchanger(source_factoobj)
---     if not source_factoobj or not source_factoobj.valid then return end 
---     if not self.factoobj or not self.factoobj.valid then
---         log(">...................................")
---         log(tostring(self.factoobj.valid))
---         log(serpent.block(self))
---         return 
---     end 
---     local id =  string.format("%s_%s", self.factoobj.unit_number, source_factoobj.unit_number) 
---     return exchangerFactory:get(id)    
--- end 
-
---- Clone door from the old surface area.
--- @tparam class<CarriageDoor> source_door
--- @tparam table props properties of the door
-function CargoWagon:createExchangerByClone(source_exchanger, props)
-    local exchanger = exchangerFactory:create(source_exchanger.type, props)
-    self.exchangers[tostring(exchanger:getId())] = exchanger
-    self.exchangers[tostring(source_exchanger:getId())] = nil
-    exchangerFactory:remove(source_exchanger)
-end 
-
+--- Build exchangers.
+-- @tparam table tiles a reference table for caching tiles
+-- @tparam table lazycalls a reference table for caching closures
 function CargoWagon:buildExchangers(tiles, lazycalls)
     local area = self:getBuildingArea()
     local ltx, lty, rbx, rby = area.left_top.x, area.left_top.y, area.right_bottom.x, area.right_bottom.y 
@@ -93,6 +79,11 @@ function CargoWagon:buildExchangers(tiles, lazycalls)
     end 
 end 
 
+--- Create exchanger.
+-- @tparam table props exchanger properties
+-- @tparam LuaPosition position exchanger position
+-- @tparam table tiles a reference table for caching tiles
+-- @tparam table lazycalls a reference table for caching closures
 function CargoWagon:createExchanger(props, position, tiles, lazycalls)
     local exchanger, addedcall = exchangerFactory:createLazyAdded("cargo-wagon-exchanger", props)
     exchanger:build(position, tiles, lazycalls, function() 
@@ -101,6 +92,17 @@ function CargoWagon:createExchanger(props, position, tiles, lazycalls)
     end)
 end 
 
+--- Clone exchanger from the old surface area.
+-- @tparam class<CargoWagonExchanger> source_exchanger
+-- @tparam table props properties of the exchanger
+function CargoWagon:createExchangerByClone(source_exchanger, props)
+    local exchanger = exchangerFactory:create(source_exchanger.type, props)
+    self.exchangers[tostring(exchanger:getId())] = exchanger
+    self.exchangers[tostring(source_exchanger:getId())] = nil
+    exchangerFactory:remove(source_exchanger)
+end 
+
+--- Clear carriage data.
 function CargoWagon:destroy()
     self.super.destroy(self)
     for id, exchanger in pairs(self.exchangers) do 
